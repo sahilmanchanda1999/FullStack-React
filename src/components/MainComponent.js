bbde012b152810aa199c9c4140427c97cd56845e
Sahil <sahilmanchanda92@gmail.com>
2020-07-10 21:29:00 +0530

    assignment-2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import {DISHES} from '../shared/dishes';
 import {COMMENTS} from '../shared/comments';
 import {PROMOTIONS} from '../shared/promotions';
 import {LEADERS} from '../shared/leaders';
+import About from './AboutComponent';
 
 import {Switch,Route,Redirect} from 'react-router-dom'; 
 
@@ -43,6 +44,7 @@ class Main extends Component {
         />
       );
     }
+    
   return (
     <div>
       <Header />
@@ -51,6 +53,7 @@ class Main extends Component {
         <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes}/>}/>
         <Route path="/menu/:dishId" component={DishWithId}/>
         <Route exact path="/contactus" component={Contact}/>
+        <Route exact path="/aboutus" component={()=><About leaders={this.state.leaders}/>}/>
         <Redirect to="/home"/>
       </Switch>
       <Footer />
