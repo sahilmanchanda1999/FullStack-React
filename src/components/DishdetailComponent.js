3fa4a94e5b0200a847e9f9743e890d6f0b1b02a3
Sahil <sahilmanchanda92@gmail.com>
2020-07-21 22:55:08 +0530

    redux-thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Card, CardImg, CardText,Button, CardBody, CardTitle,Breadcrumb,BreadcrumbItem,Modal,ModalBody,ModalHeader,Row,Col,Label} from 'reactstrap';
 import {Control,LocalForm,Errors} from 'react-redux-form';
 import {Link} from 'react-router-dom';
+import {Loading} from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength= (len) => (val)=> !(val) || (val.length<=len);
@@ -113,46 +114,67 @@ const minLength= (len) => (val)=> (val) && (val.length>=len);
         }
     }
     const DishDetail =(props)=>{
-        const dish=props.dish;
-        if(dish==null){
+        if(props.isLoading){
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Loading/>
+                    </div>
+                </div>
+            );
+            
+        }
+        else if(props.errMess){
+            return (
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if(props.dish!=null){
+            return (
+                <div className="container">
+                    <div className="row">
+                            <Breadcrumb>
+                                
+                                <BreadcrumbItem>
+                                    <Link to='/menu'>Menu</Link>
+                                </BreadcrumbItem>
+                                
+                                <BreadcrumbItem active>
+                                    {props.dish.name}
+                                </BreadcrumbItem>
+                            </Breadcrumb>
+                            <div className="col-12">
+                                <h3>{props.dish.name}</h3>
+                            </div>
+                    </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish}/>   
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} 
+                            addComment={props.addComment}
+                            dishId={props.dish.id}  />  
+                        
+    
+                    </div>
+    
+    
+                </div>
+                </div>
+                
+            );
+        }
+        else {
+
             return (
                 <div></div>
             );
         }
-        return (
-            <div className="container">
-                <div className="row">
-                        <Breadcrumb>
-                            
-                            <BreadcrumbItem>
-                                <Link to='/menu'>Menu</Link>
-                            </BreadcrumbItem>
-                            
-                            <BreadcrumbItem active>
-                                {props.dish.name}
-                            </BreadcrumbItem>
-                        </Breadcrumb>
-                        <div className="col-12">
-                            <h3>{props.dish.name}</h3>
-                        </div>
-                </div>
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    <RenderDish dish={props.dish}/>   
-                </div>
-                <div className="col-12 col-md-5 m-1">
-                    <RenderComments comments={props.comments} 
-                        addComment={props.addComment}
-                        dishId={props.dish.id}  />  
-                    
-
-                </div>
-
-
-            </div>
-            </div>
-            
-        );
     }
 
 export default DishDetail;
