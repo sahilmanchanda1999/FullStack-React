e43707c836880e47553bf3671b171a5a327e10ce
Sahil <sahilmanchanda92@gmail.com>
2020-07-24 21:50:03 +0530

    Updates

---
+++ precache-manifest.998b963dc70feeb32df504002707c238.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6d587168f1475587ca7350316b14ab",
+    "url": "/FullStack-React/index.html"
+  },
+  {
+    "revision": "a8d146259f543fa12f84",
+    "url": "/FullStack-React/static/css/2.ac72a6a1.chunk.css"
+  },
+  {
+    "revision": "ce89bcb10c7216822bec",
+    "url": "/FullStack-React/static/css/main.435aefd3.chunk.css"
+  },
+  {
+    "revision": "a8d146259f543fa12f84",
+    "url": "/FullStack-React/static/js/2.698c06d7.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/FullStack-React/static/js/2.698c06d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce89bcb10c7216822bec",
+    "url": "/FullStack-React/static/js/main.e9b258b6.chunk.js"
+  },
+  {
+    "revision": "c34d3c9cb3bf4d245be4",
+    "url": "/FullStack-React/static/js/runtime-main.a6afb75e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/FullStack-React/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/FullStack-React/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/FullStack-React/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/FullStack-React/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/FullStack-React/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
